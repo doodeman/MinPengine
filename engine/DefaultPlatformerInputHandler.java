ef8c9f5e1e29a237877b520e382e80ea43089efc
Kristleifur <batwigg@gmail.com>
2014-03-31 15:19:58 +0000

    added standard platformer input handler

---
+++ engine/DefaultPlatformerInputHandler.java
@@ -0,0 +1,38 @@
+package engine;
+
+import com.badlogic.gdx.Input.Keys;
+
+import engine.objects.Player;
+
+
+/**
+ * A default control scheme for a basic platformer. 
+ * Move left and right using the arrow keys, jump with spacebar
+ * @author kristleifur
+ *
+ */
+public class DefaultPlatformerInputHandler extends InputHandler {
+	Player player; 
+	
+	public DefaultPlatformerInputHandler(Player player) {
+		super(); 
+		this.player = player; 
+	}
+	
+	@Override
+	public boolean keyDown(int keyCode) {
+		if (keyCode == Keys.LEFT) {
+			player.velocity.x -= 0.1; 
+			return true; 
+		}
+		if (keyCode == Keys.RIGHT) {
+			player.velocity.x += 0.1; 
+			return true; 
+		}
+		if (keyCode == Keys.SPACE) {
+			player.velocity.y = 1; 
+			return true; 
+		}
+		return false; 
+	}
+}
--- engine/InputHandler.java
+++ engine/InputHandler.java
@@ -1,5 +1,61 @@
 package engine;
 
-public abstract class InputHandler {
+import java.util.ArrayList;
+import java.util.List;
+
+import com.badlogic.gdx.InputProcessor;
+
+public abstract class InputHandler implements InputProcessor {
+	List<Integer> pressedKeys;
+	
+	public InputHandler() {
+		pressedKeys = new ArrayList<Integer>(); 
+	}
+	
+	@Override 
+	public boolean keyUp(int keyCode) {
+		return false; 
+	}
+	
+	@Override
+	public boolean keyDown(int keyCode) {
+		return false; 
+	}
+
+	@Override
+	public boolean keyTyped(char arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int arg0, int arg1, int arg2, int arg3) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int arg0, int arg1, int arg2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int arg0, int arg1, int arg2, int arg3) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
