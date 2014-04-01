8e41762a439ecf7d9f72f7e5e437edb7a402b7ed
geirmj11 <geirmj11@ru.is>
2014-04-01 05:17:37 +0000

    Added some things, am now creating objects from the json files, and making new objects. The build is broken, mostly because we don't have sprites or graphics. But the object part is totally working. woop woop.

---
+++ engine/objects/StaticObject.java
@@ -0,0 +1,24 @@
+package engine.objects;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class StaticObject extends GameObject {
+
+	public StaticObject(GsonMap input, Vector2 location) {
+		super(input, location);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update(float delta) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onCollide(GameObject that) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
