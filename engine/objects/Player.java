1e021a8df20c3b781012689a38f9e722db86c77c
Kristleifur <batwigg@gmail.com>
2014-04-02 16:01:19 +0000

    oops, reenabled gravity

--- engine/objects/Player.java
+++ engine/objects/Player.java
@@ -14,6 +14,6 @@ public class Player extends MovableObject {
 	@Override
 	public void update(float delta) {
 		super.update(delta);
-		//this.velocity.y -= (float) this.map.gravity * delta; 
+		this.velocity.y -= (float) this.map.gravity * delta; 
 	}
 }
